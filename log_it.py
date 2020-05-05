c3c72cb66ef3a8c079e564dbd2d792ef00727b37
Alex Tomkins <alexctomkins@gmail.com>
2020-05-05 09:28:42 -0400

    fixed file watcher

--- log_it.py
+++ log_it.py
@@ -1,7 +1,10 @@
 import logging
+import conf
+
+filename = conf.LOCAL_DIR+'/py_converter.log'
+logging.basicConfig(filename=filename, filemode='a',level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger('requests').setLevel(logging.CRITICAL)
 
 def get_logger(name):
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logging.getLogger('requests').setLevel(logging.CRITICAL)
     return logging.getLogger(name)
 
