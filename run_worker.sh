9f6e6271f71d4acdece36589f54b8120c8e7f179
Alex Tomkins <alexctomkins@gmail.com>
2020-05-10 20:00:24 -0400

    less procs

--- run_worker.sh
+++ run_worker.sh
@@ -3,4 +3,4 @@
 SCRIPT_PATH=$(dirname "$(realpath "$0")")
 . "$SCRIPT_PATH/env/bin/activate"
 cd "$SCRIPT_PATH"
-celery -A app multi $1 w_all_hosts w_pis -Q:w_all_hosts q_all_hosts -c:w_all_hosts 3 -Q:w_pis q_pis -c:w_pis 1 --pidfile=/var/tmp/%n.pid --logfile=/var/tmp/%n.log
+celery -A app multi $1 w_all_hosts w_pis -Q:w_all_hosts q_all_hosts -c:w_all_hosts 2 -Q:w_pis q_pis -c:w_pis 1 --pidfile=/var/tmp/%n.pid --logfile=/var/tmp/%n.log
